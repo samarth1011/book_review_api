5c289b4e26cd9d29a45f98c24858c4c1e534c943
unknown <samarth.mailme@gmail.com>
2025-05-28 00:27:06 +0530

    added vercel deploy changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
